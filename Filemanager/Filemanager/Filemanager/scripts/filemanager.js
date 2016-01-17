50cb4d910a21a9cbd5651e2c8151670b7b6ac901
11BREATH11 <11breath11@gmail.com>
2016-01-17 15:04:50 +0200

    Version2

--- Filemanager/Filemanager/Filemanager/scripts/filemanager.js
+++ Filemanager/Filemanager/Filemanager/scripts/filemanager.js
@@ -2354,12 +2354,10 @@
         var ua = window.navigator.userAgent;
         var msie = ua.indexOf("MSIE ");
 
-        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer, return version number
-            return true
-        else                 // If another browser, return 0
-            return false
-
-        return false;
+        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) // If Internet Explorer, return version number
+            return true;
+        else // If another browser, return 0
+            return false;
     }
 
     function Init() {
